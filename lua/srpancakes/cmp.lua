97b7742214f99d6ade0ff04975680bc188b566dc
SrPancakes <adormilucasnunez@gmail.com>
2024-01-08 16:38:55 +0100

    added autocompletion with cmp

---
+++ lua/srpancakes/cmp.lua
@@ -0,0 +1,164 @@
+local M = {
+  "hrsh7th/nvim-cmp",
+  event = "InsertEnter",
+  dependencies = {
+    {
+      "hrsh7th/cmp-nvim-lsp",
+      event = "InsertEnter",
+    },
+    {
+      "hrsh7th/cmp-emoji",
+      event = "InsertEnter",
+    },
+    {
+      "hrsh7th/cmp-buffer",
+      event = "InsertEnter",
+    },
+    {
+      "hrsh7th/cmp-path",
+      event = "InsertEnter",
+    },
+    {
+      "hrsh7th/cmp-cmdline",
+      event = "InsertEnter",
+    },
+    {
+      "saadparwaiz1/cmp_luasnip",
+      event = "InsertEnter",
+    },
+    {
+      "L3MON4D3/LuaSnip",
+      event = "InsertEnter",
+      dependencies = {
+        "rafamadriz/friendly-snippets",
+      },
+    },
+    {
+      "hrsh7th/cmp-nvim-lua",
+    },
+  },
+}
+
+function M.config()
+  local cmp = require "cmp"
+  local luasnip = require "luasnip"
+  require("luasnip/loaders/from_vscode").lazy_load()
+  require("luasnip").filetype_extend("typescriptreact", { "html" })
+
+  local check_backspace = function()
+    local col = vim.fn.col "." - 1
+    return col == 0 or vim.fn.getline("."):sub(col, col):match "%s"
+  end
+
+  local icons = require "user.icons"
+
+  cmp.setup {
+    snippet = {
+      expand = function(args)
+        luasnip.lsp_expand(args.body) -- For `luasnip` users.
+      end,
+    },
+    mapping = cmp.mapping.preset.insert {
+      ["<C-k>"] = cmp.mapping(cmp.mapping.select_prev_item(), { "i", "c" }),
+      ["<C-j>"] = cmp.mapping(cmp.mapping.select_next_item(), { "i", "c" }),
+      ["<Down>"] = cmp.mapping(cmp.mapping.select_next_item(), { "i", "c" }),
+      ["<Up>"] = cmp.mapping(cmp.mapping.select_prev_item(), { "i", "c" }),
+      ["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-1), { "i", "c" }),
+      ["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(1), { "i", "c" }),
+      ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
+      ["<C-e>"] = cmp.mapping {
+        i = cmp.mapping.abort(),
+        c = cmp.mapping.close(),
+      },
+      -- Accept currently selected item. If none selected, `select` first item.
+      -- Set `select` to `false` to only confirm explicitly selected items.
+      ["<CR>"] = cmp.mapping.confirm { select = true },
+      ["<Tab>"] = cmp.mapping(function(fallback)
+        if cmp.visible() then
+          cmp.select_next_item()
+        elseif luasnip.expandable() then
+          luasnip.expand()
+        elseif luasnip.expand_or_jumpable() then
+          luasnip.expand_or_jump()
+        elseif check_backspace() then
+          fallback()
+        else
+          fallback()
+        end
+      end, {
+        "i",
+        "s",
+      }),
+      ["<S-Tab>"] = cmp.mapping(function(fallback)
+        if cmp.visible() then
+          cmp.select_prev_item()
+        elseif luasnip.jumpable(-1) then
+          luasnip.jump(-1)
+        else
+          fallback()
+        end
+      end, {
+        "i",
+        "s",
+      }),
+    },
+    formatting = {
+      fields = { "kind", "abbr", "menu" },
+      format = function(entry, vim_item)
+        vim_item.kind = icons.kind[vim_item.kind]
+        vim_item.menu = ({
+          nvim_lsp = "",
+          nvim_lua = "",
+          luasnip = "",
+          buffer = "",
+          path = "",
+          emoji = "",
+        })[entry.source.name]
+
+        if entry.source.name == "emoji" then
+          vim_item.kind = icons.misc.Smiley
+          vim_item.kind_hl_group = "CmpItemKindEmoji"
+        end
+
+        return vim_item
+      end,
+    },
+    sources = {
+      { name = "copilot" },
+      { name = "nvim_lsp" },
+      { name = "luasnip" },
+      { name = "cmp_tabnine" },
+      { name = "nvim_lua" },
+      { name = "buffer" },
+      { name = "path" },
+      { name = "calc" },
+      { name = "emoji" },
+      { name = "treesitter" },
+      { name = "crates" },
+      { name = "tmux" },
+    },
+    confirm_opts = {
+      behavior = cmp.ConfirmBehavior.Replace,
+      select = false,
+    },
+    window = {
+      completion = {
+        border = "rounded",
+        winhighlight = "Normal:Pmenu,CursorLine:PmenuSel,FloatBorder:FloatBorder,Search:None",
+        col_offset = -3,
+        side_padding = 1,
+        scrollbar = false,
+        scrolloff = 8,
+      },
+      documentation = {
+        border = "rounded",
+        winhighlight = "Normal:Pmenu,FloatBorder:FloatBorder,Search:None",
+      },
+    },
+    experimental = {
+      ghost_text = false,
+    },
+  }
+end
+
+return M
