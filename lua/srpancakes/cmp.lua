a5fde1eb4d7848df847cc24ba257427c65103bbf
SrPancakes <adormilucasnunez@gmail.com>
2024-01-08 16:51:41 +0100

    fine-tuned little details on cmd.lua

--- lua/srpancakes/cmp.lua
+++ lua/srpancakes/cmp.lua
@@ -145,15 +145,10 @@ function M.config()
     window = {
       completion = {
         border = "rounded",
-        winhighlight = "Normal:Pmenu,CursorLine:PmenuSel,FloatBorder:FloatBorder,Search:None",
-        col_offset = -3,
-        side_padding = 1,
         scrollbar = false,
-        scrolloff = 8,
       },
       documentation = {
         border = "rounded",
-        winhighlight = "Normal:Pmenu,FloatBorder:FloatBorder,Search:None",
       },
     },
     experimental = {
