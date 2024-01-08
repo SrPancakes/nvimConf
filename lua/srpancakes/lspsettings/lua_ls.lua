e255bc4127cc76a0175cf0192b77c870300d79f9
SrPancakes <adormilucasnunez@gmail.com>
2024-01-08 16:14:50 +0100

    created lspsettings folder and lua_ls.lua

---
+++ lua/srpancakes/lspsettings/lua_ls.lua
@@ -0,0 +1,39 @@
+-- https://luals.github.io/wiki/settings/
+
+return {
+  settings = {
+    Lua = {
+      format = {
+        enable = false,
+      },
+      diagnostics = {
+        globals = { "vim", "spec" },
+      },
+      runtime = {
+        version = "LuaJIT",
+        special = {
+          spec = "require",
+        },
+      },
+      workspace = {
+        checkThirdParty = false,
+        library = {
+          [vim.fn.expand "$VIMRUNTIME/lua"] = true,
+          [vim.fn.stdpath "config" .. "/lua"] = true,
+        },
+      },
+      hint = {
+        enable = false,
+        arrayIndex = "Disable",
+        await = true,
+        paramName = "Disable",
+        paramType = true,
+        semicolon = "All",
+        setType = false,
+      },
+      telemetry = {
+        enable = false,
+      },
+    },
+  },
+}
