1d7e995f7c84ab8ce7525054c7c30fb78edea041
SrPancakes <adormilucasnunez@gmail.com>
2024-01-09 21:16:18 +0100

    added telescope file browser extension and configured keymap to <Leader>fd

--- lua/srpancakes/comment.lua
+++ lua/srpancakes/comment.lua
@@ -1,17 +1,12 @@
 local M = {
-  'numToStr/Comment.nvim',
-  event = 'VeryLazy',
+  "numToStr/Comment.nvim",
+  event = "VeryLazy",
   lazy = false,
-  dependencies = {
-    {
-      'JoosepAlviste/nvim-ts-context-commentstring',
-      event = 'VeryLazy',
-    },
-  },
 }
 
 function M.config()
-  require('Comment').setup {
+  ---@diagnostic disable: assign-type-mismatch
+  require("Comment").setup {
     ---Add a space b/w comment and the line
     padding = true,
     ---Whether the cursor should stay at its position
@@ -21,25 +16,25 @@ function M.config()
     ---LHS of toggle mappings in NORMAL mode
     toggler = {
       ---Line-comment toggle keymap
-      line = 'gcc',
+      line = "gcc",
       ---Block-comment toggle keymap
-      block = 'gbc',
+      block = "gbc",
     },
     ---LHS of operator-pending mappings in NORMAL and VISUAL mode
     opleader = {
       ---Line-comment keymap
-      line = 'gc',
+      line = "gc",
       ---Block-comment keymap
-      block = 'gb',
+      block = "gb",
     },
     ---LHS of extra mappings
     extra = {
       ---Add comment on the line above
-      above = 'gcO',
+      above = "gcO",
       ---Add comment on the line below
-      below = 'gco',
+      below = "gco",
       ---Add comment at the end of line
-      eol = 'gcA',
+      eol = "gcA",
     },
     ---Enable keybindings
     ---NOTE: If given `false` then the plugin won't create any mappings
@@ -51,7 +46,7 @@ function M.config()
     },
     ---Function to call before (un)comment
     pre_hook = function(...)
-      local loaded, ts_comment = pcall(require, 'ts_context_commentstring.integrations.comment_nvim')
+      local loaded, ts_comment = pcall(require, "ts_context_commentstring.integrations.comment_nvim")
       if loaded and ts_comment then
         return ts_comment.create_pre_hook()(...)
       end
--- lua/srpancakes/telescope.lua
+++ lua/srpancakes/telescope.lua
@@ -1,79 +1,82 @@
 local M = {
-  "nvim-telescope/telescope.nvim",
-  dependencies = { { "nvim-telescope/telescope-fzf-native.nvim", build = "make", lazy = true } },
+  'nvim-telescope/telescope.nvim',
+  dependencies = {
+    { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make', lazy = true },
+    { 'nvim-telescope/telescope-file-browser.nvim', lazy = true },
+  },
   lazy = true,
-  cmd = "Telescope",
+  cmd = 'Telescope',
 }
 
 function M.config()
-  local icons = require "srpancakes.icons"
-  local actions = require "telescope.actions"
+  local icons = require 'srpancakes.icons'
+  local actions = require 'telescope.actions'
 
-  require("telescope").setup {
+  require('telescope').setup {
     defaults = {
-      prompt_prefix = icons.ui.Telescope .. " ",
-      selection_caret = icons.ui.Forward .. " ",
-      entry_prefix = "   ",
-      initial_mode = "insert",
-      selection_strategy = "reset",
-      path_display = { "smart" },
+      prompt_prefix = icons.ui.Telescope .. ' ',
+      selection_caret = icons.ui.Forward .. ' ',
+      entry_prefix = '   ',
+      initial_mode = 'insert',
+      selection_strategy = 'reset',
+      path_display = { 'smart' },
       color_devicons = true,
-      set_env = { ["COLORTERM"] = "truecolor" },
+      set_env = { ['COLORTERM'] = 'truecolor' },
       sorting_strategy = nil,
       layout_strategy = nil,
       layout_config = {},
       vimgrep_arguments = {
-        "rg",
-        "--color=never",
-        "--no-heading",
-        "--with-filename",
-        "--line-number",
-        "--column",
-        "--smart-case",
-        "--hidden",
-        "--glob=!.git/",
+        'rg',
+        '--color=never',
+        '--no-heading',
+        '--with-filename',
+        '--line-number',
+        '--column',
+        '--smart-case',
+        '--hidden',
+        '--glob=!.git/',
       },
 
       mappings = {
         i = {
-          ["<C-n>"] = actions.cycle_history_next,
-          ["<C-p>"] = actions.cycle_history_prev,
+          ['<C-n>'] = actions.cycle_history_next,
+          ['<C-p>'] = actions.cycle_history_prev,
 
-          ["<C-j>"] = actions.move_selection_next,
-          ["<C-k>"] = actions.move_selection_previous,
+          ['<C-j>'] = actions.move_selection_next,
+          ['<C-k>'] = actions.move_selection_previous,
         },
         n = {
-          ["<esc>"] = actions.close,
-          ["j"] = actions.move_selection_next,
-          ["k"] = actions.move_selection_previous,
-          ["q"] = actions.close,
+          ['<esc>'] = actions.close,
+          ['j'] = actions.move_selection_next,
+          ['k'] = actions.move_selection_previous,
+          ['q'] = actions.close,
         },
       },
     },
     pickers = {
       live_grep = {
-        theme = "dropdown",
+        theme = 'dropdown',
       },
 
       grep_string = {
-        theme = "dropdown",
+        theme = 'dropdown',
       },
 
       find_files = {
-        theme = "dropdown",
+        theme = 'dropdown',
         previewer = false,
       },
 
       buffers = {
-        theme = "dropdown",
+        theme = 'dropdown',
         previewer = false,
-        initial_mode = "normal",
+        initial_mode = 'normal',
         mappings = {
           i = {
-            ["<C-d>"] = actions.delete_buffer,
+            ['<C-d>'] = actions.delete_buffer,
           },
           n = {
-            ["dd"] = actions.delete_buffer,
+            ['dd'] = actions.delete_buffer,
           },
         },
       },
@@ -88,23 +91,23 @@ function M.config()
       },
 
       lsp_references = {
-        theme = "dropdown",
-        initial_mode = "normal",
+        theme = 'dropdown',
+        initial_mode = 'normal',
       },
 
       lsp_definitions = {
-        theme = "dropdown",
-        initial_mode = "normal",
+        theme = 'dropdown',
+        initial_mode = 'normal',
       },
 
       lsp_declarations = {
-        theme = "dropdown",
-        initial_mode = "normal",
+        theme = 'dropdown',
+        initial_mode = 'normal',
       },
 
       lsp_implementations = {
-        theme = "dropdown",
-        initial_mode = "normal",
+        theme = 'dropdown',
+        initial_mode = 'normal',
       },
     },
     extensions = {
@@ -112,10 +115,13 @@ function M.config()
         fuzzy = true, -- false will only do exact matching
         override_generic_sorter = true, -- override the generic sorter
         override_file_sorter = true, -- override the file sorter
-        case_mode = "smart_case", -- or "ignore_case" or "respect_case"
+        case_mode = 'smart_case', -- or "ignore_case" or "respect_case"
       },
+
     },
   }
+
+  require('telescope').load_extension 'file_browser'
 end
 
 return M
